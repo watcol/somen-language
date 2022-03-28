06473ce2da854df84a1d3c8ea5369c19aa19caf7
watcol <potfman@gmail.com>
2022-03-28 19:20:48 +0900

    Improved the float parser.

--- src/literal/numeric/float.rs
+++ src/literal/numeric/float.rs
@@ -1,10 +1,10 @@
 //! Parsers for floating point decimals.
-use core::ops::{Add, Mul, Neg};
-use num_traits::{Pow, Zero};
+use core::ops::{Mul, Neg};
+use num_traits::Pow;
 use somen::prelude::*;
 
-use super::integer::{integer, integer_trailing_zeros};
-use super::{digit, signed};
+use super::integer::{integer_inner, integer_trailing_zeros};
+use super::{digit, non_zero_digit, signed};
 use crate::Character;
 
 /// A floating point number.
@@ -12,47 +12,56 @@ use crate::Character;
 /// Note that this function doesn't support infinities or `NaN`s, you must implement it by
 /// yourself.
 // TODO: Implement Eisel-Lemire algolithm.
-#[cfg(feature = "alloc")]
 pub fn float<'a, N, I, C>(neg: bool) -> impl Parser<I, Output = N> + 'a
 where
-    N: Zero
-        + Add<N, Output = N>
-        + Pow<i32, Output = N>
-        + Mul<N, Output = N>
-        + Neg<Output = N>
-        + TryFrom<u64>
-        + Clone
-        + 'a,
+    N: Mul<N, Output = N> + Neg<Output = N> + Pow<i64, Output = N> + TryFrom<u64> + 'a,
     I: Input<Ok = C> + 'a,
     C: Character + 'a,
 {
-    let integer = integer(10, false);
-    let fraction = is(Character::is_point)
-        .expect("a decimal point")
-        .prefix(digit(10).repeat(1..))
-        .try_fold::<_, _, &str>(value((N::zero(), 0)), |(acc, offset), digit: C| {
-            Ok((
-                acc + N::try_from(10).or(Err("a valid float"))?.pow(offset - 1)
-                    * N::try_from(digit.to_digit_unchecked(10) as u64).or(Err("a valid float"))?,
-                offset - 1,
-            ))
-        })
-        .map(|(frac, _)| frac);
-    let exponent = is(Character::is_exp)
-        .expect("a exponent mark")
-        .prefix(signed(|neg| integer_trailing_zeros(10, neg), true));
-
-    (integer, fraction.opt(), exponent.opt()).try_map::<_, _, &str>(move |(int, frac, exp)| {
-        let mut int = N::try_from(int).or(Err("a not too large number."))?;
-        if let Some(f) = frac {
-            int = int + f;
-        }
-        if let Some(e) = exp {
-            int = int * N::try_from(10).or(Err("a valid float"))?.pow(e);
-        }
-        if neg {
-            int = -int;
-        }
-        Ok(int)
+    float_parser().try_map(move |(mantissa, exponent, _)| {
+        N::try_from(mantissa)
+            .ok()
+            .zip(N::try_from(10).ok())
+            .map(|(m, ten)| {
+                let n = m * ten.pow(exponent);
+                if neg {
+                    -n
+                } else {
+                    n
+                }
+            })
+            .ok_or("a valid float")
     })
+}
+
+pub fn float_parser<'a, I, C>() -> impl Parser<I, Output = (u64, i64, bool)> + 'a
+where
+    I: Input<Ok = C> + 'a,
+    C: Character + 'a,
+{
+    integer_inner::<u64, _, _, _>(
+        (non_zero_digit(10).once(), digit(10).repeat(..))
+            .or(is(Character::is_zero).expect("zero").once()),
+        0,
+        10,
+        false,
+    )
+    .then(|(int, overflowed, _)| {
+        if overflowed {
+            value((int, true, 0)).left()
+        } else {
+            is(Character::is_point)
+                .expect("a decimal point")
+                .prefix(integer_inner(digit(10).repeat(1..), int, 10, false))
+                .or(value((int, false, 0)))
+                .right()
+        }
+    })
+    .and(
+        is(Character::is_exp)
+            .expect("a exponent mark")
+            .prefix(signed(|neg| integer_trailing_zeros(10, neg), true))
+            .or(value(0)),
+    )
+    .map(|((mantissa, overflowed, count), exp2)| (mantissa, exp2 - (count as i64), overflowed))
 }
